1021bdb342cad00f7c90e907d10119b8443602aa
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-02-20 08:48:04 +0100

    change : request

--- dashboard/dash_app.py
+++ dashboard/dash_app.py
@@ -1,26 +1,14 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import pickle
+import json
+import requests
 
-st.title('🤑  Dashboard : Customer Loan')
-log_reg_pickle = open('model_saved_loan.pickle', 'rb')
-cust_info_pickle=open('cust`omers_info','rb')
-log_reg=pickle.load(log_reg_pickle)
-customer_infos=pickle.load(cust_info_pickle)
-nb_customer=np.unique(customer_infos['SK_ID_CURR'])
-customer_number=st.selectbox('Customer Number',options=nb_customer)
-customer_annuity=customer_infos[customer_infos['SK_ID_CURR']==customer_number]['AMT_ANNUITY']
-credit_customer=customer_infos[customer_infos['SK_ID_CURR']==customer_number]['AMT_CREDIT']
-st.write('Credit :'+"${:,.2f}".format(credit_customer.iloc[0]))
-st.write('Annuity :'+"${:,.2f}".format(customer_annuity.iloc[0]))
+st.title('Dashboard')
 
+# Taking id client input
+id_client = st.text_input('Customer id')
+id_client = int(id_client)
+inputs={"customer":id_client}
 
-# if st.button("Refresh"):
-#     st.experimental_rerun()
-
-
-
-
-
-
+if st.button('GoGoGo'):
+    res = requests.post(url= ('http://127.0.0.1:8000/gender'), data = json.dumps(inputs))
+    st.write(f"Customer gender = {res}")
