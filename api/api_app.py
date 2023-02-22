d28bcadae079fe346e5778d248fd427c2ea9fd1f
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-02-22 11:55:23 +0100

    add financial info

--- api/api_app.py
+++ api/api_app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 
-
+df = pd.read_csv('C:/Users/td/implement_scoring_loan/notebook/data.csv')
 class User_input(BaseModel):
     customer : int
 
@@ -10,5 +10,12 @@ app = FastAPI()
 
 @app.post('/gender')
 def prediction(input:User_input):
-    # result = df[df['SK_ID_CURR']==input.customer]['CODE_GENDER'].astype(float)
-    return {'gender':0}
+    result = df[df['SK_ID_CURR']==input.customer]['CODE_GENDER'].astype(float)
+    return {'gender':result}
+
+@app.post('/financial')
+def prediction(input:User_input):
+    income = df[df['SK_ID_CURR']==input.customer]['AMT_INCOME_TOTAL'].astype(float)
+    credit = df[df['SK_ID_CURR']==input.customer]['AMT_CREDIT'].astype(float)
+    annuity = df[df['SK_ID_CURR'] == input.customer]['AMT_ANNUITY'].astype(float)
+    return {'income':income, 'credit':credit,'annuity': annuity}
--- dashboard/dash_app.py
+++ dashboard/dash_app.py
@@ -2,13 +2,22 @@ import streamlit as st
 import json
 import requests
 
-st.title('Dashboard')
+st.title('🔮Dashboard 🔮')
 
 # Taking id client input
 id_client = st.text_input('Customer id')
-id_client = int(id_client)
-inputs={"customer":id_client}
 
 if st.button('GoGoGo'):
-    res = requests.post(url= ('http://127.0.0.1:8000/gender'), data = json.dumps(inputs))
-    st.write(f"Customer gender = {res}")
+    m1, m2, m3, m4 = st.columns((1, 1, 1, 1))
+    id_client = int(id_client)
+    inputs = {"customer": id_client}
+    res = requests.post(url= ('http://127.0.0.1:8000/gender'), json = inputs)
+    gender_value = res.json()
+    st.write(f"Customer gender = {gender_value}")
+    ## Request for financial information
+    financial = requests.post(url= ('http://127.0.0.1:8000/financial'), json = inputs)
+    financial = financial.json()
+    m1.write('')
+    m2.metric(label='Customer Income', value='{:,.0f} $'.format(financial['income']['0']))
+    m3.metric(label='Customer Credit', value='{:,.0f} $'.format(financial['credit']['0']))
+    m4.metric(label='Customer Annuity', value='{:,.0f} $'.format(financial['annuity']['0']))
