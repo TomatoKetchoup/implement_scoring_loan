b91cdc079ea3bcaaa07e3e3e88f82cd0970ffad5
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-03-12 17:47:59 +0100

    add index customer

--- api/api_app.py
+++ api/api_app.py
@@ -2,24 +2,24 @@ import pandas as pd
 from fastapi import FastAPI
 import pickle
 import joblib
-import numpy as np
+from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
 with open('C:/Users/td/implement_scoring_loan/api/explainer.pkl', 'rb') as f:
     explainer = pickle.load(f)
+with open('C:/Users/td/implement_scoring_loan/api/selected_feature_names.pkl', 'rb') as f:
+    features = list(pickle.load(f))
 
 model_saved = joblib.load('C:/Users/td/implement_scoring_loan/api/scoring_loan.joblib')
 
-with open('C:/Users/td/implement_scoring_loan/dashboard/selected_feature_names.pkl', 'rb') as f:
-    selected_feature_names = pickle.load(f)
-
+data_df = pd.read_csv('C:/Users/td/implement_scoring_loan/api/data_cleaned.csv')
+class User_input(BaseModel):
+    customer : int
 app = FastAPI()
 
 @app.post("/local_importance")
-def process_data():
-    data_customer = np.array([0.00000000e+00, -9.46100000e+03, -6.37000000e+02, 8.30369674e-02,
-                              2.62948593e-01, 1.39375780e-01, 0.00000000e+00, 0.00000000e+00,
-                              -1.43700000e+03, -8.74000000e+02, -4.99875000e+02, 2.50000000e-01,
-                              7.50000000e-01, 1.00000000e+00, 0.00000000e+00, 1.00000000e+00])
+def process_data(input:User_input):
+    index_client = data_df[data_df['SK_ID_CURR'] == input.customer].index[0]
+    data_customer = data_df[features].values[index_client]
     exp = explainer.explain_instance(data_customer, model_saved.set_params(selector=None).predict_proba, num_features=5, num_samples=5)
     return JSONResponse(content={"result": exp.as_html()})
