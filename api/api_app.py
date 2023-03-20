6bf1f8cba4ee75c8d420260ea13edb8a4a1cf676
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-03-20 09:34:20 +0100

    fix version

--- api/api_app.py
+++ api/api_app.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 import pickle
 import joblib
 from pydantic import BaseModel
-from fastapi.responses import JSONResponse
 
-with open('C:/Users/td/implement_scoring_loan/api/explainer.pkl', 'rb') as f:
+
+with open('explainer.pkl', 'rb') as f:
     explainer = pickle.load(f)
-with open('C:/Users/td/implement_scoring_loan/api/selected_feature_names.pkl', 'rb') as f:
+with open('selected_feature_names.pkl', 'rb') as f:
     features = list(pickle.load(f))
 
-model_saved = joblib.load('C:/Users/td/implement_scoring_loan/api/scoring_loan.joblib')
+model_saved = joblib.load('scoring_loan.joblib')
 
-data_df = pd.read_csv('C:/Users/td/implement_scoring_loan/api/df_api.csv',nrows=10)
+data_df = pd.read_csv('df_api.csv',nrows=10)
 class User_input(BaseModel):
     customer : int
 app = FastAPI()
@@ -21,12 +21,7 @@ app = FastAPI()
 def process_data(input:User_input):
     index_client = data_df[data_df['SK_ID_CURR'] == input.customer]
     data_customer = data_df[features].values[index_client]
-<<<<<<< Updated upstream
-    exp = explainer.explain_instance(index_client, model_saved.set_params(selector=None).predict_proba, num_features=5, num_samples=5)
-    probabilities = model_saved.predict_proba(index_client)[0].tolist()
-    html_explanation = exp.as_html(show_all=False)
-    return JSONResponse(content={"result": {"probabilities": probabilities, "html_explanation": html_explanation}, "predict_proba": False})
-=======
+
     exp = explainer.explain_instance(data_customer, model_saved.set_params(feature_selection=None).predict_proba, num_features=5, num_samples=5)
     probabilities = exp.predict_proba.tolist()
     feature_explanation = exp.as_map()
@@ -36,7 +31,6 @@ def process_data(input:User_input):
     feature_names = [float(x) for x in feature_names]
     return {"probabilities": probabilities,'importances': feature_importance,'features':feature_names}
 
->>>>>>> Stashed changes
 
 @app.post('/financial')
 def prediction(input:User_input):
