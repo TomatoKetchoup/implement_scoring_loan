5f35c58c32b3d562f7dcf80828f4d6ab22605884
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-04-03 13:28:48 +0200

    path

--- tests/api/test_appli.py
+++ tests/api/test_appli.py
@@ -1,11 +1,24 @@
-from api.api_app import read_main
+import sys
+import os
+from fastapi.testclient import TestClient
 import json
 import pathlib
-from fastapi.testclient import TestClient
-api_dir = pathlib.Path(__file__).parent.resolve()
-print(api_dir)
 
-client = TestClient(read_main)
+# Ajouter le chemin absolu du répertoire contenant l'application
+api_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'api'))
+sys.path.append(api_dir)
+
+# Importer l'application FastAPI
+from api_app import app
+
+# Créer un objet TestClient
+client = TestClient(app)
+
+# Importer les données de test
+data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'data_to_test.json'))
+with open(data_path, 'r') as f:
+    data_to_test = json.load(f)
+
 def test_read_main():
     response = client.get("/test")
     assert response.status_code == 200
