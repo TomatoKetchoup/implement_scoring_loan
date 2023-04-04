ac5f5275471ef7c9ec0af837dad7cbedc91da58b
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-04-04 20:31:23 +0200

    test hello

--- tests/api/test_appli.py
+++ tests/api/test_appli.py
@@ -16,19 +16,11 @@ client = TestClient(app)
 
 # Importer les données de test
 data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'data_to_test.json'))
-with open(data_path, 'r') as f:
-    data_to_test = json.load(f)
+
 
 def test_read_main():
     response = client.get("/test")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
-# import features to be tested
-with open(api_dir/'../data/data_to_test.json', 'r') as f:
-    data_to_be_tested = json.load(f)
 
-def test_exp_score():
-    response = client.post('/prediction', json=data_to_be_tested)
-
-    assert response.status_code == 200
