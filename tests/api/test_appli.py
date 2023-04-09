6123fa9536632260c0ed70172840d1e326941116
TomatoKetchoup <anne.desmedt@gmx.fr>
2023-04-09 16:24:58 +0200

    pytest

--- tests/api/test_appli.py
+++ tests/api/test_appli.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+import json
 from fastapi.testclient import TestClient
 
 # Ajouter le chemin relatif du répertoire contenant l'application
@@ -12,7 +13,17 @@ from api_app import app
 # Créer un objet TestClient
 client = TestClient(app)
 
+# Data to test
+data_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/data_to_test.json'))
+with open(data_file, 'r') as f:
+    data_to_test = json.load(f)
+
 def test_read_main():
     response = client.get("/test")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+def test_api_prediction():
+    response = client.post('/prediction', json=data_to_test)
+    assert response.status_code == 200
+    assert 'predict_proba' in response.json()
